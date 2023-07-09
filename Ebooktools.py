96167282f6da7e358a5820aa6530470e26c8bcf4
chingming <chingmingyao@gmail.com>
2023-07-09 20:58:17 +0800

    自動截圖電子書、轉PDF檔及ui界面

--- Ebooktools.py
+++ Ebooktools.py
@@ -1,12 +1,17 @@
-class Ebooktools():
+from PyQt5 import QtCore
+class Ebooktools(QtCore.QObject):
     """
     電子書截圖及轉檔工具
     """
+    progress_updated = QtCore.pyqtSignal(int)
     def __init__(self):
-        pass
+        super().__init__()
 
 
-    def sceenshottojpg(self,outputpath,pagecount):
+    def screenshottojpg(self,outputpath:str,pagecount:int):
+        """
+        預設將hyread的雙頁電子書存檔
+        """
         self.outputpath = outputpath
         self.pagecount = pagecount
         import time
@@ -71,40 +76,62 @@ class Ebooktools():
 
             # 等待五秒钟
             time.sleep(3)
+            
+            # 发出进度信号，将当前迭代作为参数传递
+            self.progress_updated.emit(i+1)
 
-        def jpgtopdf(self,inputpath,outputpath):
-            self.inputpath = inputpath
-            self.outputpath = outputpath
-            from PIL import Image
-            from fpdf import FPDF
-            import glob
+    def jpgtopdf(self,inputpath:str,pdf_filename:str):
+        """
+        將指定資料夾中的jpg檔合併成一個pdf檔
+        jpgtopdf("C:\\Python 邁向數據科學之路 ChatGPT助攻\\","Python 邁向數據科學之路 ChatGPT助攻")
+        output目錄需加\\ 
+        """
+        self.inputpath = inputpath
+        self.pdf_filename = pdf_filename
 
-            # 图像文件夹路径和输出PDF文件路径
-            image_folder = "C:/1234/"
-            pdf_filename = "output.pdf"
+        from PIL import Image
+        from fpdf import FPDF
+        import glob
 
-            # 创建PDF对象
-            pdf = FPDF(orientation="L", unit="pt")
+        # 图像文件夹路径和输出PDF文件路径
+        image_folder = inputpath #"C:/1234/"
+        pdf_filename = self.pdf_filename #"output.pdf"
 
-            # 获取图像文件列表
-            image_files = glob.glob(image_folder + "*.jpg")
+        # 创建PDF对象
+        """将上述代码中的 orientation 参数值从 "L" 改为 "P" 即可将页面方向设置为纵向。
 
-            # 遍历图像文件列表并将图像添加到PDF中
-            for image_file in image_files:
-                # 打开图像文件
-                image = Image.open(image_file)
-                
-                # 调整图像大小以适应PDF页面
-                max_width = 842  # PDF页面的宽度
-                max_height = 595  # PDF页面的高度
-                image.thumbnail((max_width, max_height))
-                
-                # 获取调整后的图像尺寸
-                width, height = image.size
-                
-                # 添加PDF页面并将图像添加到页面上
-                pdf.add_page()
-                pdf.image(image_file, x=0, y=0, w=width, h=height)
+            另外，请确保在添加图像到 PDF 页面时，调整图像大小的代码部分仍使用固定的宽度和高度（1000*740）："""
+        pdf = FPDF(orientation="P", unit="pt")
 
-            # 保存PDF文件
-            pdf.output(pdf_filename, "F")
+       
+
+        # 获取图像文件列表
+        image_files = glob.glob(image_folder + "*.jpg")
+
+        # 遍历图像文件列表并将图像添加到PDF中
+        count=0
+        for image_file in image_files:
+            # 打开图像文件
+            count+=1
+            image = Image.open(image_file)
+            
+            # 调整图像大小以适应PDF页面
+            max_width = 620 #2000 # PDF页面的宽度
+            max_height = 860 #1482  # PDF页面的高度 數字越小範圍越大
+            image.thumbnail((max_width, max_height))
+            
+            # 获取调整后的图像尺寸
+            width, height = image.size
+
+            
+            # 添加PDF页面并将图像添加到页面
+            pdf.add_page()
+            pdf.image(image_file, x=0, y=0, w=width, h=height)
+            self.progress_updated.emit(count + 1)
+        # 保存PDF文件
+        pdf.output(pdf_filename + ".pdf", "F")
+
+if __name__ == "__main__":
+    ebook = Ebooktools()
+    ebook.jpgtopdf("C:\\Python 邁向數據科學之路 ChatGPT助攻\\","Python 邁向數據科學之路 ChatGPT助攻")
+    print("ok")
